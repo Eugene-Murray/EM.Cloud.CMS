5ac94f2297c5e229ed1f4c32a8413deb96e75ddb
Eugene-Murray <eugene.murray@gmail.com>
2014-08-06 21:21:44 +0100

    Create and add files

---
+++ CMS.Models/ModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Models
+{
+    public class ModelBase
+    {
+        [DisplayName("Created On")]
+        public DateTime? CreatedOn { get; set; }
+
+        [DisplayName("Modified On")]
+        public DateTime? ModifiedOn { get; set; }
+
+        [DisplayName("Created On"), StringLength(256)]
+        public string CreatedBy { get; set; }
+
+        [DisplayName("Modified On"), StringLength(256)]
+        public string ModifiedBy { get; set; }
+    }
+}
