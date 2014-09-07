15461a5ce68865bdf7de6e7d37b7f9c06508ba99
Eugene-Murray <eugene.murray@gmail.com>
2014-09-07 21:47:05 +0100

    update for wrapstrap

--- App/shell.js
+++ App/shell.js
@@ -8,10 +8,12 @@
            return router.map([
               { route: ['home', ''], title: 'Home', moduleId: 'templatePage/index', nav: true },
               { route: 'addOrder', moduleId: 'addOrder/index', title: 'Add Order', nav: true },
+              { route: 'masterBook', moduleId: 'masterBook/index', title: 'Master Book', nav: true },
+              { route: 'altHomePages', moduleId: 'altHomePages/index', title: 'Alternative Home Pages', nav: true },
               { route: 'inputWorkflow*details', title: 'Workflow', moduleId: 'inputWorkflow/index', nav: true, hash: '#inputWorkflow' },
               { route: 'siteConfiguration', title: 'Site Configuration', moduleId: 'siteConfiguration/index', nav: false },
               { route: 'about', title: 'About', moduleId: 'About/about', nav: true },
-              { route: 'test', title: 'Test', moduleId: 'Test/index', nav: true }
+              { route: 'test', title: 'Examples', moduleId: 'Test/index', nav: true }
            ]).buildNavigationModel()
              .mapUnknownRoutes('home', "#home")
              .activate();
